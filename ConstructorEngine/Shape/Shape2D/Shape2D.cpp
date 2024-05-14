d3d19548a8d8d927560495a48f6854a9bbc7e4bb
Amirka04 <kokovamir6@gmail.com>
2024-05-14 13:59:11 +0300

    Добавление абстрактного класса фигуры

---
+++ ConstructorEngine/Shape/Shape2D/Shape2D.cpp
@@ -0,0 +1,11 @@
+#include "Shape2D.h"
+
+Shape2D::Shape2D()
+{
+    
+}
+
+Shape2D::~Shape2D()
+{
+
+}
---
+++ ConstructorEngine/Shape/Shape2D/Shape2D.h
@@ -0,0 +1,21 @@
+#ifndef SHAPE2D_H
+#define SHAPE2D_H
+
+#include <memory.h>
+#include "window.h"
+
+
+class Shape2D
+{
+public:
+    // В конструкторе я буду принимать адрес своего же класса как родитель от которого будет наследоватся
+    // поворот, позиция и трансформация
+    Shape2D();
+    ~Shape2D();
+    
+private:
+    
+};
+
+
+#endif

2f4ee42d27643bb378cf69ec020709bed5d5478c
Amirka04 <kokovamir6@gmail.com>
2024-05-29 13:20:59 +0300

    добавление системы управления индексами VBO

--- Example/ShapeRender/main.cpp
+++ Example/ShapeRender/main.cpp
@@ -1,5 +1,6 @@
 #include "window.h"
 #include "event.h"
+#include "BufferManager.h"
 
 #include "Shape2D/Shape2D.h"
 #include "Rectangle/Rectangle.hpp"
@@ -12,22 +13,27 @@ int main(int args, char** argv){
     assert(window::initialization("RenderShape", 900, 900) == true);
     event::init();
 
-    Circle *circle = new Circle(glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f));
+    Rectangle *shape = new Rectangle(glm::vec2(0.0f, 0.0f), glm::vec2(0.5f));
+    shape->setColor(glm::vec3(GREEN));
 
+    int i = 0;
 
     while(!window::isShouldCloseWindow()){
         window::clear(glm::vec4(0.5f));
         
-        
+        shape->setRotate( ++i % 360 );
+        shape->render();
+                
+
         if(event::jKeyPressed(GLFW_KEY_Q))
             window::setShouldCloseWindow(true);
-        
-
+            
         window::swapBuffers();
         event::update();
     }
 
-
+    // очистка всех ресурсов
+    BufferManager::finallization();
     event::free();
     window::finallyzation();
 }

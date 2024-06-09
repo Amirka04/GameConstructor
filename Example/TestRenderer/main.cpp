e903359c96e5893457739b78f49c539e46b73a1f
Amirka04 <kokovamir6@gmail.com>
2024-06-09 22:25:36 +0300

    Изменение примера использования

--- Example/TestRenderer/main.cpp
+++ Example/TestRenderer/main.cpp
@@ -13,25 +13,18 @@ int main(int args, char** argv){
     event::init();
     window::setIcon("source/GameConstructorIcon.png");
 
-    Rectangle *shape = new Rectangle(glm::vec2(0.0f, 0.0f), glm::vec2(0.25f));
-    shape->setColor(GREEN);
 
-    Rectangle *shape2 = new Rectangle(glm::vec2(1.0f), glm::vec2(0.25f));
-    shape->setColor(WHITE);
+    Rectangle *shape = new Rectangle(glm::vec2(-0.2), glm::vec2(0.25f));
+    shape->setColor(glm::vec3(BLACK));
 
+    Rectangle *shape2 = new Rectangle(glm::vec2(0.2), glm::vec2(0.25f));
+    shape2->setColor(GREEN);
 
+    
     while(!window::isShouldCloseWindow()){
         window::clear(glm::vec4(0.5f));
         
         Renderer::render();
-
-
-        if(event::jKeyPressed(GLFW_KEY_S))
-            shape->show();
-        if(event::jKeyPressed(GLFW_KEY_H))
-            shape->hide();
-        if(event::jKeyPressed(GLFW_KEY_D))
-            delete shape;
 
         if(event::jKeyPressed(GLFW_KEY_Q))
             window::setShouldCloseWindow(true);

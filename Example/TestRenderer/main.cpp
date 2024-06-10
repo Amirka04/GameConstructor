f6dabbab54ff11f712ff2d1724f72ce6ff3bab66
Amirka04 <kokovamir6@gmail.com>
2024-06-10 19:59:04 +0300

    Изменение примера

--- Example/TestRenderer/main.cpp
+++ Example/TestRenderer/main.cpp
@@ -7,6 +7,15 @@
 
 
 
+void swapPos(Rectangle& shape, Rectangle& shape2){
+    glm::vec2 tmp = shape.getPosition();
+    shape.setPosition(shape2.getPosition());
+    shape2.setPosition(tmp);
+}
+
+
+
+
 int main(int args, char** argv){
     assert(window::initialization("RenderShape", 900, 900) == true);
     Renderer::initialization();
@@ -14,17 +23,19 @@ int main(int args, char** argv){
     window::setIcon("source/GameConstructorIcon.png");
 
 
-    Rectangle *shape = new Rectangle(glm::vec2(-0.2), glm::vec2(0.25f));
-    shape->setColor(glm::vec3(BLACK));
+    Rectangle shape(glm::vec2(0.0f, 0.0f), glm::vec2(0.1f));
+    shape.setColor(glm::vec3(BLUE));
 
-    Rectangle *shape2 = new Rectangle(glm::vec2(0.2), glm::vec2(0.25f));
-    shape2->setColor(GREEN);
 
-    
     while(!window::isShouldCloseWindow()){
-        window::clear(glm::vec4(0.5f));
-        
+        window::clear(glm::vec4(0.5f));            
+
         Renderer::render();
+
+        if(event::jKeyPressed(GLFW_KEY_S))
+        shape.show();
+        if(event::jKeyPressed(GLFW_KEY_H))
+            shape.hide();
 
         if(event::jKeyPressed(GLFW_KEY_Q))
             window::setShouldCloseWindow(true);
